89af81ea753b4c5399133b3d5179570fd0321f39
bigbearishappy <953308023@qq.com>
2022-03-01 08:54:06 +0000

    use the python-can lib to test the CAN interface

--- samples/test_can.py
+++ samples/test_can.py
@@ -1,26 +1,39 @@
-#!/usr/bin/env python3
-
-import os 
+# NOTICE: please make sure you have pip3 install python-can
+#         before you use this test script
+# import the library
+import can
 import sys
 import time
 
 param1 = sys.argv[1]
 
+# create a bus instance
+# many other interfaces are supported as well (see documentation)
+bus = can.Bus(interface='socketcan',
+              channel='can0',
+              receive_own_messages=True)
+
 if param1 == "send":
+    # send a message
     counter=0
     print("can send starts now!\n")
     try:
-        while counter < 10:
-            os.system('cansend can0 111#0' + str(counter))
+        while True:
+            message = can.Message(arbitration_id=123, is_extended_id=True,
+                      data=[0x11, 0x22, counter])
+            bus.send(message, timeout=0.2)
             time.sleep(1)
             counter += 1
     except KeyboardInterrupt:
         exit()
+
 elif param1 == "receive":
-    print("can start receiving data now!\n")
+    # iterate over received messages
     try:
-        os.system('candump can0')
+        for msg in bus:
+            print(f"{msg.arbitration_id:X}: {msg.data}")
     except KeyboardInterrupt:
         exit()
 else:
     print('param input error,try again with send or receive')
+
